eb673a31e0eea749884fc70d99c120307591f5e5
Alex Stecuk <sandrostecuk@gmail.com>
2019-03-14 23:33:25 +0200

    initialize repositiory

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="css/style.css">
+    
+    </head>
+    <body>
+       <header>
+
+       <form  class="search-form" action="" method="post">
+       <input type="text" class="search">
+       <input type="text" class="submit-search">
+       </form>
+       <nav class="nav-menu">
+       <ul class="list-menu">
+           <li>Головна</li>
+           <li>Предмети</li>
+           <li>Цікавинки</li>
+           <li>Дошкільнята</li>
+           <li>Розвиваючі ігри</li>
+       </ul>
+       </nav>
+       </header>
+
+    </body>
+</html>
