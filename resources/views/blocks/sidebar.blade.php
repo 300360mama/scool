628cbf0b681405fc8a618c59d003075998c83644
Alex Stecuk <sandrostecuk@gmail.com>
2019-05-01 00:18:59 +0300

    update(*): add awesome fonts, style sidebar,

--- resources/views/blocks/sidebar.blade.php
+++ resources/views/blocks/sidebar.blade.php
@@ -1,37 +1,74 @@
 @section('sidebar')
-    <aside>
-        <section class="about-me">
-            <span class="aside-title">About Me</span>
-            <span class="about-txt">
-                Ever since I was a little girl I have been deeply passionate about food. My childhood was                      largely spent scurrying between the restaurants of Hong Kong, where I grew up,
-            </span>
+<aside>
+    <section class="about-me aside-section">
+        <span class="aside-title">About Me</span>
+        <img src="./image/sidebar/girl1.png" class="about-logo" alt=" image">
+        <span class="about-txt">
+            Ever since I was a little girl I have been deeply passionate about food.
+            My childhood was largely spent
+            scurrying between the restaurants of Hong Kong, where I grew up,
+            <br>
             <a class="read-more" href="#">Read More</a>
-        </section>
-        <section class="follow-us">
-            <span class="aside-title">Subscribe & Follow</span>
-            <div class="social-block">
-                <a href="#" class="aside-social fa-twitter"></a>
-                <a href="#" class="aside-social fa-facebook "></a>
-                <a href="#" class="aside-social fa-instagram"></a>
-                <a href="#" class="aside-social fa-telegram"></a>
+        </span>
+
+
+    </section>
+    <section class="follow-us aside-section">
+        <span class="aside-title">Subscribe & Follow</span>
+        <div class="social-block">
+            <a href="#" class="aside-social fab fa-twitter"></a>
+            <a href="#" class="aside-social fab fa-facebook-f "></a>
+            <a href="#" class="aside-social fab fa-instagram"></a>
+            <a href="#" class="aside-social fab fa-telegram"></a>
+        </div>
+    </section>
+    <section class="pinterest aside-section">
+        <span class="aside-title">Pinterest</span>
+        <div class="pinterest-imgs">
+            <img src="./image/sidebar/image1.png" alt="">
+            <img src="./image/sidebar/image1.png/" alt="">
+            <img src="./image/sidebar/image1.png" alt="">
+            <img src="./image/sidebar/image1.png" alt="">
+            <img src="./image/sidebar/image1.png" alt="">
+            <img src="./image/sidebar/image1.png" alt="">
+        </div>
+    </section>
+    <section class="latest-post aside-section">
+        <span class="aside-title">Latest Post</span>
+        <div class="latest-post-list">
+            <div class="latest-post-block">
+                <img src="./image/sidebar/image1.png" alt="" class="post-id-logo">
+                <div class="post-id">
+                    <span class="post-id-text">Moxine Headphone by Andy Meerow</span>
+                    <span class="post-id-date">November 16, 2015 </span>
+                </div>
             </div>
-        </section>
-        <section class="pinterest">
-            <span class="aside-title">Pinterest</span>
-            <div class="pinterest-imgs">
-                <img src="" alt="">
-                <img src="" alt="">
-                <img src="" alt="">
-                <img src="" alt="">
-                <img src="" alt="">
-                <img src="" alt="">
+            <div class="latest-post-block">
+                <img src="./image/sidebar/image1.png" alt="" class="post-id-logo">
+                <div class="post-id">
+                    <span class="post-id-text">Moxine Headphone by Andy Meerow</span>
+                    <span class="post-id-date">November 16, 2015 </span>
+                </div>
             </div>
-        </section>
-        <section class="latest-post">
-            <span class="aside-title">Latest Post</span>
-        </section>
-        <section class="tags">
-            <span class="aside-title">Tags</span>
-        </section>
-    </aside>
+            <div class=latest-post-block>
+                <img src="./image/sidebar/image1.png" alt="" class="post-id-logo">
+                <div class="post-id">
+                    <span class="post-id-text">Moxine Headphone by Andy Meerow</span>
+                    <span class="post-id-date">November 16, 2015 </span>
+                </div>
+            </div>
+            <div class="latest-post-block">
+                <img src="./image/sidebar/image1.png" alt="" class="post-id-logo">
+                <div class="post-id">
+                    <span class="post-id-text">Moxine Headphone by Andy Meerow</span>
+                    <span class="post-id-date">November 16, 2015 </span>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section class="tags aside-section ">
+        <span class="aside-title">Tags</span>
+    </section>
+</aside>
 @endsection
