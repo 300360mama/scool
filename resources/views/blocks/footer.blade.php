5fbbf25803463becbfb4f6a53cc9b5dc7dbd9dad
Стецюк Олександр <sandrostecuk@gmail.com>
2019-08-04 22:50:11 +0300

    style.cssfooter.blade.php

--- resources/views/blocks/footer.blade.php
+++ resources/views/blocks/footer.blade.php
@@ -1,6 +1,6 @@
 @section('footer')
 <footer>
-        <div class="footer-image">
+        <div class="footer_image">
             <img src="./image/footer/image1.png" alt="">
             <img src="./image/footer/image2.png" alt="">
             <img src="./image/footer/image3.png" alt="">
@@ -8,7 +8,7 @@
             <img src="./image/footer/image5.png" alt="">
             <img src="./image/footer/image6.png" alt="">
         </div>
-        <div class="opacity-wrapper"></div>
+        <div class="opacity_wrapper"></div>
         <span class="footer-copyright">&copy; Copyright <span class="date">2019</span></span>
 </footer>
 
