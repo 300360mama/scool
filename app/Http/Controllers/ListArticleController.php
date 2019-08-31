cb07b1a3d479ce30b50e558a9a9b6cf35949c4f1
Стецюк Олександр <sandrostecuk@gmail.com>
2019-08-31 16:20:28 +0300

    add Article model,,fix style

--- app/Http/Controllers/ListArticleController.php
+++ app/Http/Controllers/ListArticleController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ListArticleController extends Controller
 {
     public function index() {
-        return view('list_article');
+
+
+        $articles = Article::all();
+        return view('list_article', ['articles'=>$articles]);
     }
 }
--- resources/views/list_article.blade.php
+++ resources/views/list_article.blade.php
@@ -6,53 +6,25 @@
 
 @section('content')
 <main>
-    <article>
-    
-        <h3 class="title">LIst article</h3>
-        <span class="article_category">Lifestyle</span>
-        <span class="article_date">November 16, 2015</span>
-        <img src="./image/content/article-img.jpg" alt="logo-article" class="article_logo">
-        <span class="article_text">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-            sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
-            magna aliquam erat volutpat. Ut wisi enim ad minim veniam, 
-            quis nostrud exerci tation ullamcorper suscipit lobortis nisl 
-            ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
-            dolor in hendrerit in vulputate velit esse molestie consequat, 
-            vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan 
-            et iusto odio dignissim qui blandit praesent luptatum zzril delenit
-            augue duis dolore te feugait nulla facilisi. Nam liber tempor cum 
-            soluta nobis eleifend option congue nihil imperdiet doming id 
-            quod mazim placerat facer possim assum. 
 
-            Typi non habent claritatem insitam; est usus legentis in iis qui 
-            facit eorum claritatem. Investigationes demonstraverunt lectores 
-            legere me lius quod ii legunt saepius. Claritas est etiam processus 
-            dynamicus, qui sequitur mutationem consuetudium lectorum. 
-            Mirum est notare quam littera gothica, quam nunc putamus parum 
-            claram, anteposuerit litterarum formas humanitatis per seacula 
-            quarta decima et quinta decima. Eodem modo typi, qui nunc nobis
-             videntur parum clari, fiant sollemnes in futurum.
+    @foreach($articles as $article)
 
-            Duis autem vel eum iriure dolor in hendrerit in vulputate velit
-            esse molestie consequat, vel illum dolore eu feugiat nulla 
-            facilisis at vero eros et accumsan et iusto odio dignissim qui 
-            blandit praesent luptatum zzril delenit augue duis dolore te feugait 
-            nulla facilisi. Nam liber tempor cum soluta nobis eleifend option 
-            congue nihil imperdiet doming id quod mazim placerat facer possim assum. 
-             
-            Claritas est etiam processus dynamicus, qui sequitur mutationem 
-            consuetudium lectorum. Mirum est notare quam littera gothica, 
-            quam nunc putamus parum claram, anteposuerit litterarum formas 
-            humanitatis per seacula quarta decima et quinta decima. 
-            Eodem modo typi, qui nunc nobis videntur parum clari, 
-            fiant sollemnes in futurum.
-        </span>
+        <article>
 
-        <a href="#" class="continue_read">Продовжити читання 
-            <span class="fa fa-arrow-right"></span>
-        </a>
-    </article>
+            <h3 class="title">{{ $article->title_article }}</h3>
+            <span class="article_category">Lifestyle</span>
+            <span class="article_date">{{ $article->created_at }}</span>
+            <img src="./image/content/article-img.jpg" alt="logo-article" class="article_logo">
+            <span class="article_text">
+                {{ $article->content_article }}
+            </span>
+
+            <a href="#" class="continue_read">Продовжити читання
+                <span class="fa fa-arrow-right"></span>
+            </a>
+        </article>
+    @endforeach
+
 
     @yield('like_article')
 
