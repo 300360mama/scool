698f5c8af3b562a17af3f7ee1016acd973f0eec4
Alexandr Stecuk <sandrostecuk@gmail.com>
2020-06-17 09:44:18 +0300

    Revert "update(web.php): route"
    This reverts commit 3b9977de6b763f6cdb95126792282f100c755538.

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::post('/crud/show/{table}/{page?}', 'CrudController@show')->name("show");
 Route::post('/crud/all', 'CrudController@index')->name("all");
 
 
-Route::get('/scool', 'VueController@index');
-Route::get('/sool/{category}', 'VueController@index');
-Route::get('/sool/items/{subcategory}/{id}', 'ArticlesController@index')->name("subcategory");
-Route::get('/sool/crud/show/{table}/{page?}', 'CrudController@show')->name("show");
-Route::get('/sool/crud/all', 'CrudController@index')->name("all");
+Route::get('/', 'VueController@index');
+Route::get('/{category}', 'VueController@index');
+Route::get('/items/{subcategory}/{id}', 'ArticlesController@index')->name("subcategory");
+Route::get('/crud/show/{table}/{page?}', 'CrudController@show')->name("show");
+Route::get('/crud/all', 'CrudController@index')->name("all");
 
